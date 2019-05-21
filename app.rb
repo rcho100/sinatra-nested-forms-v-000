5285f191128ffc7c301b581ebbebfaf453a89a1f
Richard Cho <richardcho100@gmail.com>
2019-05-21 00:49:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,14 +12,15 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
+      attributes = params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height]
+      @pirate = Pirate.new(params[:pirate], params)
 
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
 
       @ships = Ship.all
-      
+
       erb :'pirates/show'
     end
   end
