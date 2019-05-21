08920c5fb58c819ed13b06a213589cccaa2b8016
Richard Cho <richardcho100@gmail.com>
2019-05-21 01:35:50 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,21 +1,21 @@
 class Ship
   attr_reader :name, :type, :booty
 
-  @all = []
+  @@all = []
 
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
     @booty = args[:booty]
-    @all << self
+    @@all << self
   end
 
   def self.all
-    @all
+    @@all
   end
 
   def self.clear
-    @all = []
+    @@all = []
   end
 
 end
